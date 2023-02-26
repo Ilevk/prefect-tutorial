e2e8f34b06f1af291b5a541d9e2d14f74fa9cb69
ilevk <kcs93023@gmail.com>
2023-02-26 13:01:46 +0900

    feat - modify start_mlflow.sh

--- start_mlflow.sh
+++ start_mlflow.sh
@@ -4,6 +4,6 @@ export AWS_ACCESS_KEY_ID=mlflow_admin
 export AWS_SECRET_ACCESS_KEY=mlflow_admin
 export MLFLOW_S3_ENDPOINT_URL=http://localhost:9000
 
-mlflow server --port 5001 --backend-store-uri postgresql://postgres:postgres@localhost:5433/postgres --default-artifact-root s3://mlflow/mlruns
+mlflow server --port 5000 --backend-store-uri postgresql://postgres:postgres@localhost:5432/mlflow_db --default-artifact-root s3://mlflow/mlruns
 
 docker-compose down
